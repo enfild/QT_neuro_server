7c4d1286367a936cb9971c0deb2b5342914a9746
Paul <paul.sirenko@yandex.ru>
2020-07-03 15:39:11 +0700

    add read coordinates in C++

--- include/NeuronetMaster.h
+++ include/NeuronetMaster.h
@@ -2,17 +2,27 @@
 #define TF_using_HEADER
 #define QT_NO_KEYWORDS
 #undef slots
+//#define PY_SSIZE_T_CLEAN
 #include "Python.h"
 #include <QDebug>
 #include <QObject>
+#include <QEventLoop>
 #include <QtNetwork/QTcpSocket>
 #include <QtNetwork/QUdpSocket>
 #include <QtNetwork/QTcpServer>
 #include "QFileSystemWatcher"
 #include <QtNetwork/QHostAddress>
 #include <QTimer>
+#include <QRegularExpression>
 
 #define slots
+
+namespace  {
+    int countCath;
+    // координаты катетера
+    int x;
+    int y;
+}
 
 class NeuronetMaster: public QObject
 {
@@ -25,6 +35,11 @@ public:
     ~NeuronetMaster();
 
 private:
+    PyObject *pModule; //create main module
+
+    PyObject *main_dict;
+
+    PyObject *translation;
 
     const QString imagePath = "sad";
 
@@ -38,7 +53,19 @@ private:
     int server_status;
 
     /// \ Инициализация Графа, загрузка его в память
-    static void TF_init();
+    void TF_init();
+
+    /// \ Обработка кадра инструментами Питона
+    void TF_processing();
+
+    ///\ подстраховка
+    static bool TF_done();
+
+    /// \ чтение точек
+    QString pointReader(PyObject *ItemString);
+
+    /// \ парсер строки
+    void parserString(QString inString);
 
     /// \ Инициализация сокета
     bool connectSocket();
@@ -51,13 +78,6 @@ private:
     /// \ отправка
     static void sendSocket();
 
-    /// \ Обработка кадра инструментами Питона
-    void TF_processing();
 
-    ///\ подстраховка
-    static bool TF_done();
-
-    /// \ ОТправка точек
-    static void pointSender();
 };
 #endif
